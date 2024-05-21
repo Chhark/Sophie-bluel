e1669123ba88c006f5730980703577c346568d70
Chhark <kieran.2002.30@gmail.com>
2024-05-21 14:13:59 +0200

    récup des élement

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,1 +1,14 @@
-console.log("helloworld")
+ async function test() {
+    let reponse = await fetch('http://localhost:5678/api/works')
+    pieces = await reponse.json();
+    const valeurPieces = JSON.stringify(pieces);
+    console.log("test1")
+    console.log(pieces )
+    console.log(valeurPieces)
+    console.log(pieces[2])
+    console.log("test")  
+} 
+
+test()
+    
+
