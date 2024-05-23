e2017e372900455224e95340b4211f11f5a6e9b7
Chhark <kieran.2002.30@gmail.com>
2024-05-23 17:32:21 +0200

    login fonctionnelle

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,14 +1,50 @@
- async function test() {
+let log = false
+let change  = document.querySelectorAll(".change")
+console.log(change)
+ async function affichageprojet() {
     let reponse = await fetch('http://localhost:5678/api/works')
     pieces = await reponse.json();
-    const valeurPieces = JSON.stringify(pieces);
-    console.log("test1")
-    console.log(pieces )
-    console.log(valeurPieces)
-    console.log(pieces[2])
-    console.log("test")  
+    const valeurPieces = JSON.stringify(pieces);  
+    let galery = document.querySelector(".gallery")
+
+    pieces.forEach((élement) => {
+        html = "<figure><img src=\""+élement.imageUrl+"\"alt=\""+élement.title+"\"> <figcaption>"+élement.title+"</figcaption> </figure>";
+        console.log(html)
+        galery.innerHTML += html
+ })
 } 
 
-test()
-    
+affichageprojet()
+const form = document.getElementById('login-form');
 
+form.addEventListener('submit',async (event) => {
+    event.preventDefault()
+    const Email = document.querySelector(".LogEmail").value
+    const MDP = document.querySelector(".LogMDP").value
+    let token = await fetch('http://localhost:5678/api/users/login',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify({
+            email: Email,
+            password: MDP,
+        })
+    })
+    token = await token.json()
+    console.log(token)
+    log = true
+    connectedView(log) 
+})
+
+function connectedView(loged) {
+    if (loged) {
+        console.log("vous êtes connecté")
+
+
+    }
+    else {
+        console.log("vous n'êtes pas connecté")
+    }
+
+}
