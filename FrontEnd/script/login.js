48722f84cf323b0a00e693047273c2b3fc8185c6
Chhark <kieran.2002.30@gmail.com>
2024-06-22 02:39:53 +0200

    nettoyage script

--- FrontEnd/script/login.js
+++ FrontEnd/script/login.js
@@ -5,23 +5,22 @@ form.addEventListener('submit',async (event) => {
     event.preventDefault()
     const Email = document.querySelector(".LogEmail").value
     const MDP = document.querySelector(".LogMDP").value
-    let token = await fetch('http://localhost:5678/api/users/login',{
+    let Responsetoken = await fetch('http://localhost:5678/api/users/login',{
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
             body: JSON.stringify({
             email: Email,
             password: MDP,
         })
     })
-    if (token.ok) {
-        token = await token.json()
-        window.localStorage.setItem("token",token)
-        console.log(token)
+    if (Responsetoken.ok) {
+        Responsetoken= await Responsetoken.json()
+        window.localStorage.setItem("token",JSON.stringify(Responsetoken))
+        console.log(Responsetoken)
         window.location.href = "http://127.0.0.1:5501/FrontEnd/index.html" 
         console.log("test")
-        connectedView()
     } else{
         console.log("vous n'êtes pas connecté")
         error.style.display = "block"
---
+++ FrontEnd/script/scripttest.js
@@ -0,0 +1,26 @@
+let token = window.localStorage.getItem('token')
+token = JSON.parse(token)
+console.log(token.token)
+let fillle = document.getElementById('test')
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+fillle.addEventListener('input' , async (event) =>{
+    event.preventDefault()
+    formData = new FormData()
+    const file = event.target.files[0];
+    formData.append('image', file); // Ajoute l'image
+    formData.append('title', 'Votre Titre Ici'); // Ajoute le titre
+    formData.append('category', 1);
+    const response = await fetch('http://localhost:5678/api/works',{
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token.token // Assurez-vous que le token est correct
+        },
+        body: formData
+    })
+    console.log('envoie reussi')
+    console.log(response)
+    console.log("test")
+    event.preventDefault()
+})
