aa07021b0051231c0d9b0f9f7bfd08c938ff1c15
Chhark <kieran.2002.30@gmail.com>
2024-06-23 13:51:31 +0200

    preview image

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,8 +1,19 @@
 let change_in  = document.querySelectorAll(".change-in")
 let change_out  = document.querySelectorAll(".change-out")
+let pieces
+
+async function loadCategories(){
+    let categoriesResponse = await fetch('http://localhost:5678/api/categories')
+    let categories = await categoriesResponse.json();
+    const listCategorie = JSON.stringify(categories)
+    window.localStorage.setItem("categories",listCategorie)
+}
+
+loadCategories()
+
 connectedView()
 async function affichageprojet() {
-    let pieces = window.localStorage.getItem("work");
+    pieces = window.localStorage.getItem("work");
     if (pieces === null) {
         let reponse = await fetch('http://localhost:5678/api/works')
         pieces = await reponse.json();
@@ -16,7 +27,6 @@ async function affichageprojet() {
     }
       
 } 
-
 function innerelement(pieces){
     let galeryy = document.querySelector(".gallery")
 
@@ -71,8 +81,8 @@ let BG_popup = document.querySelector(".BG-popup")
 async function galeryPhoto() {
     let galery = window.localStorage.getItem("work")
     if (galery === null) {
-         galery = await fetch('http://localhost:5678/api/works')
-         galery = await response.json();
+         Responsegalery = await fetch('http://localhost:5678/api/works')
+         galery = await Responsegalery.json();
         galery = JSON.stringify(galery);
         window.localStorage.setItem("work",galery)
         galery = JSON.parse(galery);
@@ -85,12 +95,143 @@ async function galeryPhoto() {
 }
 
 function innerGalery(galery) {
-    galery.forEach((élement) => {
-        let img = "<div class=\"relativeDiv\"><img src=\""+élement.imageUrl+"\"alt=\""+élement.title+"\"><i class=\"fa-solid fa-trash-can fa-2xs\"></i></div>"
-        display_img.innerHTML += img
-    })}
+    let Display_img = ""
+    galery.forEach((élement , index) => {
+        let img = `
+            <div class="relativeDiv">
+                <img src="${élement.imageUrl}" alt="${élement.title}">
+                <i class="fa-solid fa-trash-can fa-2xs"data-id="${index}"></i>
+            </div>
+        `;
+        Display_img += img
+    })
+    display_img.innerHTML = Display_img
+    document.querySelectorAll(".fa-trash-can").forEach(trashicon => {
+        trashicon.addEventListener("click",function() {
+            let idd = this.getAttribute('data-id');
+            console.log(pieces)
+            idd = pieces[idd].id
+            console.log(idd)
+        })
+    })
+
+}
 
 let edition = document.querySelector(".projet-edition").addEventListener("click" , () => {
     BG_popup.classList.remove("none")
     galeryPhoto()
 })
+
+document.querySelector(".addPhoto").addEventListener('click',()=>{
+    document.querySelectorAll(".popup").forEach((popup ) =>{
+        popup.classList.toggle('none')
+                
+    })
+    let optionCategorie = ""
+    let selectCategories = document.getElementById("categorie")
+    let caté = window.localStorage.getItem("categories")
+    caté = JSON.parse(caté)
+    caté.forEach((element)=>{
+        optionCategorie += ` <option value="${element.name}">${element.name}</option>`
+    })
+    selectCategories.innerHTML = optionCategorie
+    
+})
+let preview = document.querySelector(".ajout-photo")
+let image = document.querySelector('#imageUpload').files[0]
+let image2 = document.querySelector('#imageUpload')
+let file 
+let formData = new FormData();
+image2.addEventListener("change" , (event)=>{
+    file = event.target.files[0]
+
+    if(file){
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = e.target.result;
+            preview.innerHTML = `<img src="${img}" alt=""></img>`
+        };
+
+        reader.readAsDataURL(file);
+    
+    
+}
+})
+ 
+let token = window.localStorage.getItem('token')
+token = JSON.parse(token)
+document.getElementById('imageUpload').addEventListener('change',async function(event) {
+    event.preventDefault();
+    const file = event.target.files[0]; // Récupère le fichier
+    if (!file) {
+        console.log("Aucun fichier sélectionné.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', file); // Ajoute l'image
+
+    await AddAPIWork(formData)
+    
+
+
+}) 
+let formm = document.getElementById('addWork')
+let submitButton = document.querySelector(".valid")
+let RequiredInput = formm.querySelectorAll("input , select")
+console.log(formm , submitButton , RequiredInput)
+
+function FormComplet() {
+    let allfilled = true
+    console.log("form")
+    RequiredInput.forEach(input => {
+        if(!input.value){
+            allfilled = false 
+        }
+    })
+    if (allfilled) {
+        console.log("complet")
+        submitButton.classList.remove('btn-disabled');
+        submitButton.classList.add('btn-enabled');
+        submitButton.disabled = false;
+    } else {
+        console.log("incomplet")
+        submitButton.classList.remove('btn-enabled');
+        submitButton.classList.add('btn-disabled');
+        submitButton.disabled = true;
+    }
+}
+
+RequiredInput.forEach(input => {
+    input.addEventListener('input', FormComplet);
+});
+let response
+formm.addEventListener('submit', async (Event)=>{
+    Event.preventDefault()
+    let titre = document.querySelector('#Titre').value
+    let catégorie = 1 /*document.querySelector('#categorie').value*/
+    //let formData = new FormData();
+    //formData.append('image', file);
+    formData.append('title', titre);
+    formData.append('category', catégorie );
+    console.log(file)
+    console.log("test")
+    let token = window.localStorage.getItem('token')
+    console.log(token)
+    token = JSON.parse(token)
+    console.log(token)
+    publishwork(formData , token)
+    console.log("test2")
+    console.log(response)
+})
+
+async function publishwork(formData , token){
+    const response = await fetch('http://localhost:5678/api/works',{
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token.token // Assurez-vous que le token est correct
+        },
+        body: formData
+    }) 
+}
